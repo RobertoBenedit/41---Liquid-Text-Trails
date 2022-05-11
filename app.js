1dbc7ab05f1435d43695791efb9ab109bb5966e6
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-05-11 17:16:17 -0300

    update

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ ctx.textBaseline = "middle";
 let letterArray = ["R", "O", "C", "O", "M", "U", "N", "I", "C", "A"];
 let hue = 0;
 let particles = [];
-let numberOfParticles = (canvas.width * canvas.height) / 5000;
-// let numberOfParticles = 20;
+let numberOfParticles = (canvas.width * canvas.height) / 7000;
 
 const mouse = {
     x: 0,
@@ -48,11 +47,27 @@ class Particle {
         ctx.fill();
         ctx.closePath();
 
-        // ctx.beginPath();
-        ctx.fillStyle = "white";
-        ctx.strokeStyle = "black";
         ctx.font = `bold ${this.radius}px roboto`;
-        ctx.fillText(this.text, this.x - this.radius/2.7, this.y + this.radius/10);
+        ctx.fillText(
+            this.text,
+            this.x - this.radius / 2.7,
+            this.y + this.radius / 10
+        );
+    }
+    update() {
+        if (mouse.x === undefined && mouse.y === undefined) {
+            let newX =
+                ((mouse.radius * canvas.width) / 200) *
+                Math.sin(mouse.autopilotAngle * (Math.PI / 180));
+
+            let newY =
+                ((mouse.radius * canvas.height) / 200) *
+                Math.cos(mouse.autopilotAngle * (Math.PI / 180));
+
+            mouse.x = newX + canvas.width / 2;
+            mouse.y = newY + canvas.height / 2;
+        }
+        mouse.autopilotAngle += 0.05;
     }
 }
 
@@ -99,6 +114,7 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < particles.length; i++) {
         particles[i].draw();
+        particles[i].update();
     }
     if (particles.length >= numberOfParticles) {
         for (let i = 0; i < 5; i++) {
@@ -111,3 +127,24 @@ function animate() {
 }
 
 animate();
+let autopilot = setInterval(() => {
+    mouse.x = undefined;
+    mouse.y = undefined;
+}, 40);
+
+canvas.addEventListener("mouseleave", () => {
+    autopilot = setInterval(() => {
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }, 40);
+});
+
+canvas.addEventListener("mouseenter", () => {
+    clearInterval(autopilot);
+    autopilot = undefined;
+});
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
